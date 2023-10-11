ebbfe4054bf6f8976fe80cec6a57ec42bc6e5d32
gitGibbo <60328148+gitgibbo@users.noreply.github.com>
2023-10-11 13:32:11 +0100

    looped main

--- main.py
+++ main.py
@@ -16,26 +16,34 @@ config = load_configuration()
 # Set some display options
 pd.set_option('display.max_rows', None)
 
-# Clear terminal
-clear_terminal()
+while True:
 
-# Display banner
-banner()
-# Automate data collection and processing
-try:
-    data, distributor = automator(config['input_directory'])
-except Exception as e:
-    print(f"An error occurred: {e}")
-    exit(1)
+    # Clear terminal
+    clear_terminal()
 
-# Save the processed data
-if data.empty:
-    print("No data to process. Exiting.")
-    exit(1)
+    # Display banner
+    banner()
+    # Automate data collection and processing
+    try:
+        data, distributor = automator(config['input_directory'])
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        exit(1)
 
-total_invoice_amount = data["Invoice Amount"].sum()
-rounded_total_invoice_amount = math.floor(total_invoice_amount * 100) / 100
-print(f'\nThe total invoice amount for this period is £{rounded_total_invoice_amount}\n')
+    # Save the processed data
+    if data.empty:
+        print("No data to process. Exiting.")
+        exit(1)
 
-print('Saving Data!')
-print(save_dataframe(data, directory_path= config['output_directory'], custom_name=f'{distributor}_{data.iloc[0, 3]}'.lower()))
+    total_invoice_amount = data["Invoice Amount"].sum()
+    rounded_total_invoice_amount = math.floor(total_invoice_amount * 100) / 100
+    print(f'\nThe total invoice amount for this period is £{rounded_total_invoice_amount}\n')
+
+    print('Saving Data!')
+    print(save_dataframe(data, directory_path= config['output_directory'], custom_name=f'{distributor}_{data.iloc[0, 3]}'.lower()))
+
+    restart = input('Do you want to restart the tool? (y/n): ')
+
+    if restart.lower != 'y':
+        clear_terminal()
+        break
